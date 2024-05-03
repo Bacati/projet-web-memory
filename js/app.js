82e2aa9ded4b14384c52ad98fa1bf4ec5f45ef82
Bacati <lucaslievre2001@gmail.com>
2024-05-03 14:34:08 +0200

    rapideeee

--- js/app.js
+++ js/app.js
@@ -120,7 +120,7 @@ function cardsMatch(card1, card2) {
   card1.classList.add('match');
   card2.classList.add('match');
   match++;
-  if (match === 2) {
+  if (match === 1) {
       win();
   }
 }
